353d5523d57cb4e0de24a12b9c47a5b20fcabd04
Johnyrico22 <117599131+Johnyrico22@users.noreply.github.com>
2025-02-28 12:38:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { 
+  getFirestore, 
+  collection, 
+  addDoc, 
+  doc, 
+  getDoc, 
+  getDocs, 
+  query,       // <-- Import query here
+  orderBy, 
+  updateDoc 
+} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
